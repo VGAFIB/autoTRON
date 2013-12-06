8a75638ed9060d3e53f533521728b678a439765f
Daniel Martí <mvdan@mvdan.cc>
2013-12-06 13:46:01 +0100

    Don't continue if an error occurred, unchanged PATH

--- make.sh
+++ make.sh
@@ -1,10 +1,7 @@
-#!/bin/bash
-
-export PATH=$PATH
+#!/bin/bash -e
 
 NAME=`date | md5sum`
 echo $NAME
-
 
 cd multitron
 g++ -o Game_$NAME -O3 -pthread -std=c++11 BackTrace.o Utils.o Board.o Action.o Player.o Registry.o Game.o Main.o ../upload/AI$1.o ../upload/AI$2.o ../upload/AI$3.o ../upload/AI$4.o

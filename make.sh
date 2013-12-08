1b0f60439722267af0ab995cdc13d6d8beddf752
Miquel Llobet <mllobet.cm@gmail.com>
2013-12-08 13:13:02 +0100

    now it works :shipit:

--- make.sh
+++ make.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
 
-NAME=`date | md5sum`
-echo $NAME
+export PATH=$PATH
+NAME=`date | md5sum | awk '{print $1}'`
 
 cd multitron
 g++ -o Game_$NAME -O3 -pthread -std=c++11 BackTrace.o Utils.o Board.o Action.o Player.o Registry.o Game.o Main.o ../upload/AI${1}.o ../upload/AI${2}.o ../upload/AI${3}.o ../upload/AI${4}.o
 
-./Game_$NAME $1 $2 $3 $4 -i ../maps/${5}.gam -o test.t3d
+timeout 10s ../hackme/hackme ./Game_$NAME $1 $2 $3 $4 -i ../maps/${5} -o test.t3d
 
 rm Game_$NAME
